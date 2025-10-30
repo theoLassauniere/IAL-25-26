1cd268f7c83d08f64f9bc8e76022ce52dad788a1
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2025-10-30 14:12:38 +0100

    refactor: structure de données réaliste

--- poc-heart/db/init.sql
+++ poc-heart/db/init.sql
@@ -2,11 +2,16 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
 -- Exemple : création d'une table hypertable
-CREATE TABLE IF NOT EXISTS sensor_data (
+CREATE TABLE IF NOT EXISTS heart_beats (
                                            time TIMESTAMP NOT NULL,
                                            sensor_id INTEGER,
-                                           temperature DOUBLE PRECISION,
-                                           humidity DOUBLE PRECISION
+                                           heart_beats DOUBLE PRECISION,
 );
 
-SELECT create_hypertable('sensor_data', 'time', if_not_exists => TRUE);
+SELECT create_hypertable('heart_beats', 'time', if_not_exists => TRUE);
+
+-- Exemple d'insertion de données
+INSERT INTO heart_beats (time, sensor_id, heart_beats) VALUES
+('2024-01-01 00:00:00', 1, 72.5),
+('2024-01-01 00:01:00', 1, 75.0),
+('2024-01-01 00:02:00', 1, 73.8);
