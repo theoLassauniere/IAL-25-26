2a8dd328d477be9d1073946634da41f452b0e375
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2025-10-31 17:08:26 +0100

    feat: service is working on its own

--- poc-heart/backend/storage-service/src/main/java/com/storageservice/controller/HeartBeatController.java
+++ poc-heart/backend/storage-service/src/main/java/com/storageservice/controller/HeartBeatController.java
@@ -3,15 +3,21 @@ package com.storageservice.controller;
 import com.storageservice.model.HeartBeat;
 import com.storageservice.service.HeartBeatStorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/heartbeat")
-@RequiredArgsConstructor
 public class HeartBeatController {
     private final HeartBeatStorageService heartBeatStorageService;
+
+    // Mon lombok bug
+    @Autowired
+    public HeartBeatController(HeartBeatStorageService heartBeatStorageService) {
+        this.heartBeatStorageService = heartBeatStorageService;
+    }
 
     @GetMapping
     public List<HeartBeat> getAll() {
--- poc-heart/backend/storage-service/src/main/java/com/storageservice/service/HeartBeatStorageService.java
+++ poc-heart/backend/storage-service/src/main/java/com/storageservice/service/HeartBeatStorageService.java
@@ -3,14 +3,19 @@ package com.storageservice.service;
 import com.storageservice.model.HeartBeat;
 import com.storageservice.repository.HeartBeatRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class HeartBeatStorageService {
     private final HeartBeatRepository heartBeatRepository;
+
+    @Autowired
+    public HeartBeatStorageService(HeartBeatRepository heartBeatRepository) {
+        this.heartBeatRepository = heartBeatRepository;
+    }
 
     public List<HeartBeat> getAllHeartBeat() {
         return heartBeatRepository.findAll();
