b05111fd01e93680d61e707cb19848262a6870e2
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2025-11-03 10:55:13 +0100

    feat: service init

---
+++ poc-heart/backend/listener-service/src/test/java/com/listenerservice/ListenerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listenerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
